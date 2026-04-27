8c2489cdf3461560456c96d24e90861239725546
chiayu_wang <58775086+chiayu-wow@users.noreply.github.com>
2026-04-27 16:51:45 -0500

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        ## use 1 d dp
+        ## if i in coins, dp[i] = 1 if not , we iterate through its prev neighbor to find if current amount can be form
+        n = len(coins)
+        dp = [math.inf] * (amount + 1)
+        coins = set(coins)
+        print(coins)
+        if amount  == 0:
+            return 0
+
+        for i in range(1, amount+1):
+            if i in coins:
+                print("here", i)
+                dp[i] = 1
+            else:
+                for j in range(i-1, 0, -1):
+                    if i - j in coins and dp[j] != math.inf:
+                        dp[i] = min(dp[i], dp[j] + 1)
+        print(dp)
+        return dp[-1] if dp[-1] != math.inf else -1
