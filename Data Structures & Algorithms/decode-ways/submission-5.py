7b14068053249721b1349c9ae518ecbc9da6906a
chiayu_wang <58775086+chiayu-wow@users.noreply.github.com>
2026-04-27 16:35:34 -0500

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        ## 1d dp 
+        ## dp[i] number of decode end at index i
+
+        n = len(s)
+        m = len(s.lstrip("0"))
+
+        if n != m:
+            return 0
+
+        dp = [0]*(n+1)
+        
+        dp[0] = 1
+        dp[1] = 1 if s[0] != "0" else 0
+
+        for i in range(2,n+1):
+            if s[i-1] != "0":       
+                dp[i] += dp[i-1]
+
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+
+        print(dp)
+        return dp[-1]
+
+        
